b73e3849e758d03277582ccca96353a09e19d02d
“AngelReyes” <angel.ivan.konige@gmail.com>
2023-11-26 14:56:31 -0600

    agregue unos comentarios

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//aqui todas las funciones o que
+
+
+
+
+
+//y pues aqui abajito los html, supongo tendremos unas banderas para hacer de que unos include, ej: include bajas.php en una condicional o asi taltal
+?>
