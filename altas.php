db427344e66258dfe12f9f493378f24badae3276
Rodolfopz <rjunior007@live.com.mx>
2023-11-27 20:41:05 -0600

    Edit altas.php YA JALAAAAAA

--- altas.php
+++ altas.php
@@ -1,5 +1,4 @@
 <?php
-//Si es necesario le quitan la extension del puerto del localhost
 $nombre_servidor = "localhost:3310";
 $nombre_usuario = "root";
 $contraseña = "";
@@ -26,6 +25,34 @@ if ($resultado->num_rows > 0) {
 $tabla_seleccionada = null;
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['tabla'])) {
     $tabla_seleccionada = $_POST["tabla"];
+
+    $sql_columnas = "SHOW COLUMNS FROM $tabla_seleccionada";
+    $resultado_columnas = $conn->query($sql_columnas);
+
+    // Preparar los nombres de las columnas para la inserción
+    $column_names = [];
+    while ($fila = $resultado_columnas->fetch_assoc()) {
+        $column_names[] = $fila['Field'];
+    }
+
+    // Insertar datos en la tabla seleccionada
+    if (isset($_POST['submit'])) {
+        $values = [];
+        foreach ($column_names as $column) {
+            if (isset($_POST[$column])) {
+                $values[] = "'" . $conn->real_escape_string($_POST[$column]) . "'";
+            } else {
+                $values[] = "NULL";
+            }
+        }
+
+        $sql_insert = "INSERT INTO $tabla_seleccionada (" . implode(', ', $column_names) . ") VALUES (" . implode(', ', $values) . ")";
+        if ($conn->query($sql_insert) === TRUE) {
+            echo "<p>Registro insertado correctamente en la tabla $tabla_seleccionada.</p>";
+        } else {
+            echo "<p>Error al insertar el registro: " . $conn->error . "</p>";
+        }
+    }
 }
 
 $conn->close();
@@ -47,28 +74,15 @@ $conn->close();
     </form>
 
     <?php if ($tabla_seleccionada): ?>
-        <?php
-        $conn = new mysqli($nombre_servidor, $nombre_usuario, $contraseña, $nombre_base_datos);
-
-        if ($conn->connect_error) {
-            die("Conexión fallida: " . $conn->connect_error);
-        }
-
-        $sql_columnas = "SHOW COLUMNS FROM $tabla_seleccionada";
-        $resultado_columnas = $conn->query($sql_columnas);
-
-        echo "<h2>Insertar nuevo registro en $tabla_seleccionada</h2>";
-        echo "<form method='post' action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "'>";
-        echo "<input type='hidden' name='tabla' value='$tabla_seleccionada'>";
-        while ($fila = $resultado_columnas->fetch_assoc()) {
-            echo "<label>" . $fila['Field'] . ":</label>";
-            echo "<input type='text' name='" . $fila['Field'] . "'><br>";
-        }
-        echo "<input type='submit' name='submit' value='Insertar'>";
-        echo "</form>";
-
-        $conn->close();
-        ?>
+        <h2>Insertar nuevo registro en <?php echo $tabla_seleccionada; ?></h2>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <input type="hidden" name="tabla" value="<?php echo $tabla_seleccionada; ?>">
+            <?php foreach ($column_names as $column): ?>
+                <label><?php echo $column; ?>:</label>
+                <input type="text" name="<?php echo $column; ?>"><br>
+            <?php endforeach; ?>
+            <input type="submit" name="submit" value="Insertar">
+        </form>
     <?php endif; ?>
 </body>
 </html>
